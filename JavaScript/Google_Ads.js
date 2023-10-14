f7f771f5bea5c7d03ff9a2ea76982be10f15891d
kewachan <kewachan@gmail.com>
2023-10-14 15:40:15 +0800

    Update Google_Ads.js

--- JavaScript/Google_Ads.js
+++ JavaScript/Google_Ads.js
@@ -1,2 +1,13 @@
-var collection = document.getElementsByClassName("jnyxRd TpRPV");
-collection.remove();
+function ads_remove_attribute(tag, attribute, regex) {
+    const nodelist = document.querySelectorAll(tag);
+    for (i = 0; i < nodelist.length; i++) {
+        let strings = nodelist[i].getAttribute(attribute);
+        if (strings !== null) {
+            if (strings.match(regex)) {
+                nodelist[i].style.display = "none";
+            }
+        }
+    }
+}
+
+let regex = /^jnyxRd TpRPV$/g;ads_remove_attribute("div", "class", regex)
