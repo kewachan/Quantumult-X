dda8bb505d1fc37aea1123dff18ce7a112165595
kewachan <kewachan@gmail.com>
2023-10-16 15:27:52 +0800

    Update Google_Ads.js

--- JavaScript/Google_Ads.js
+++ JavaScript/Google_Ads.js
@@ -1,4 +1,4 @@
-let ele = '<head>';
-let eleReplace = '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/kewachan/Quantumult-X/main/CSS/css.css" type="text/css" /><script type="text/javascript" async="async" src="https://raw.githubusercontent.com/kewachan/Quantumult-X/main/JavaScript/googleadblock.js"></script>'
+let ele = '<body>';
+let eleReplace = '<body><link rel="stylesheet" href="https://raw.githubusercontent.com/kewachan/Quantumult-X/main/CSS/css.css" type="text/css" /><script type="text/javascript" async="async" src="https://raw.githubusercontent.com/kewachan/Quantumult-X/main/JavaScript/googleadblock.js"></script>'
 let body = $response.body.replace(ele, eleReplace) 
 $done({ body }); 
