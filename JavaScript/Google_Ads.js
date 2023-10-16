1bcb7c4ee32dcefbd27e36da04c31052221444c8
kewachan <kewachan@gmail.com>
2023-10-16 12:10:24 +0800

    Create Google_Ads.js

---
+++ JavaScript/Google_Ads.js
@@ -0,0 +1,4 @@
+let ele = '<head>';
+let eleReplace = '<head><link rel="stylesheet" href="https://limbopro.com/CSS/Adblock4limbo.user.css" type="text/css" /><script type="text/javascript" async="async" src="https://raw.githubusercontent.com/kewachan/Quantumult-X/main/JavaScript/googleadblock.js"></script>'
+let body = $response.body.replace(ele, eleReplace) 
+$done({ body }); 
