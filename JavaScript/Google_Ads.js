cb43ff5da2b5c765841bc15b6ca66c76c7d5e0d1
kewachan <kewachan@gmail.com>
2023-10-16 18:03:33 +0800

    Update Google_Ads.js

--- JavaScript/Google_Ads.js
+++ JavaScript/Google_Ads.js
@@ -1,5 +1,5 @@
-let ele = '<head>';
-let eleReplace = '<head><link rel="stylesheet" href="https://raw.githubusercontent.com/kewachan/Quantumult-X/main/CSS/css.css" type="text/css" /><script type="text/javascript" src="https://raw.githubusercontent.com/kewachan/Quantumult-X/main/JavaScript/googleadblock.js"></script>'
-let body = $response.body.replace(ele, eleReplace) 
-console.log(body) 
-$done({ body }); 
+const regex='<head>';const replace_str='<head>\
+<link rel="stylesheet" href="https://limbopro.com/CSS/Adblock4limbo.user.css" type="text/css" />\
+<script type="text/javascript" async="async" src="https://raw.githubusercontent.com/kewachan/Quantumult-X/main/JavaScript/googleadblock.js"></script>\
+'
+if($response.body){var body=$response.body.replace(regex,replace_str)};const headers=$response.headers;headers['Content-Security-Policy']='*';$done({headers:headers,body:body});
