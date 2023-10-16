3b8a92e89d05bf00d711edd77632cf671bb40c81
kewachan <kewachan@gmail.com>
2023-10-16 11:57:29 +0800

    Update googleadblock.js

--- JavaScript/googleadblock.js
+++ JavaScript/googleadblock.js
@@ -1,2 +1,10 @@
+function adbclock() {
 const node = document.querySelector(".jnyxRd.TpRPV")
 node.style.display = "none";
+}
+
+
+for (let i = 0; i < 100; i++) {
+	setTimeout(10);
+  	adbclock() ;
+}
