b6006b721be29b0c0f5f171e80fea65309342580
kewachan <kewachan@gmail.com>
2023-10-16 16:32:24 +0800

    update

--- JavaScript/googleadblock.js
+++ JavaScript/googleadblock.js
@@ -1,17 +1,5 @@
-function adbclock() {
+setTimeout (function() {
 const node = document.querySelector(".jnyxRd.TpRPV")
 node.style.display = "none";
 console.log(node) 
-}
-
-
-setTimeout(()=>{fadbclock()}, 200);
-
-
-
-
-
-for (let i = 0; i < 1000; i++) {
-	setTimeout(100);
-  	adbclock() ;
-}
+} , 3000);
