da95368d1ff502d8f495ec62cb70dc742b44433d
kewachan <kewachan@gmail.com>
2023-10-17 10:58:02 +0800

    Update googleadblock.js

--- JavaScript/googleadblock.js
+++ JavaScript/googleadblock.js
@@ -1,21 +1,16 @@
-function ads_remove_attribute(tag, attribute, regex) {
-    const nodelist = document.querySelectorAll(tag);
-    for (i = 0; i < nodelist.length; i++) {
-        let strings = nodelist[i].getAttribute(attribute);
-        if (strings !== null) {
-            if (strings.match(regex)) {
-                nodelist[i].style.display = "none";
-		console.log("Clean Finished!");
-            }
-        }
-    }
-}
-
-let regex = /^jnyxRd TpRPV$/g;
-window.onload = ads_remove_attribute("div", "class", regex);
-
-setTimeout (function() {
-ads_remove_attribute("div", "class", regex);
-} , 1000);
-
-alert("your script run");
+function adblock4limbo(){let new_body=document.createElement('body');new_body.id='newbody';let body=document.body;document.querySelector('html').appendChild(new_body);let new_a=document.createElement('a')
+new_a.id="new_a";new_a.href="https://t.me/Adblock4limbo/21";var origin='\
+    opacity: 1;\
+    width: 54px;\
+    height: 54px;\
+    bottom: 10%;\
+    right: 10px;\
+    position: fixed;\
+    border: aliceblue;\
+    background-color: transparent;\
+    background-image: url("https://limbopro.com/Adblock4limbo.svg") !important;\
+    background-size: 100% !important;\
+    background-repeat: no-repeat;\
+'
+new_a.style=origin;document.getElementById('newbody').appendChild(new_a);}
+adblock4limbo()
